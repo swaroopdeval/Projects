8e70f0517c43de38044507d4455867ecd3a8bdac
Swaroop Singh Deval <ssinghdeval@gmail.com>
2015-01-24 17:14:12 +0530

    Create login.php

---
+++ CS252/login.php
@@ -0,0 +1,51 @@
+<form name="reg" action="session.php" onsubmit="return validateForm()" method="post">
+<table width="274" border="0" align="center" cellpadding="2" cellspacing="0">
+<!--  <tr>
+    <td colspan="2">
+		<div align="center">
+		  <?php 
+		echo 'Sign in';
+		?>	
+	    </div></td>
+  </tr>-->
+  <!--  <td width="95"><div align="right">First Name:</div></td>
+    <td width="171"><input type="text" name="fname" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Last Name:</div></td>
+    <td><input type="text" name="lname" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Gender:</div></td>
+    <td><input type="text" name="mname" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Address:</div></td>
+    <td><input type="text" name="address" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Contact No.:</div></td>
+    <td><input type="text" name="contact" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Picture:</div></td>
+    <td><input type="text" name="pic" /></td>
+  </tr>
+ <tr>-->
+    <td><div align="right">Username:</div></td>
+    <td><input type="text" name="username" /></td>
+  </tr>
+ <tr>
+    <td><div align="right">Password:</div></td>
+    <td><input type="password" name="password" /></td>
+  </tr>
+  <tr>
+    <td><div align="right"><a href="signup.php">Sign Up</a></div></td>
+    <td><input name="submit" type="submit" value="Log In" /></td>
+<!--    <td>
+     <a href="signup.php">Sign Up</a>
+    </td>-->
+  </tr>
+</table>
+</form>
+
